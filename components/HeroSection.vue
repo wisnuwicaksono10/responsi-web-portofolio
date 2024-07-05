2045fad1d2f1df1fa6cadae017b1e974b896c49b
wisnu <162547014+wisnuwicaksono10@users.noreply.github.com>
2024-07-05 22:06:17 +0700

    Update HeroSection.vue

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -11,7 +11,7 @@
                   <div class="d-flex flex-column align-items-center text-center">
                     <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
                     <div class="mt-3">
-                      <h4> WISNU WITJAKONO </h4>
+                      <h4> WISNU WICAKONO 22.11.4728 </h4>
                       <p class="text-secondary mb-1">Full Stack Developer</p>
                       <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
                       <button class="btn btn-primary">Follow</button>
